a4de3394e6ca84d25a346d4733f182cb5b6a82aa
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-09-18 21:18:24 +0530

    added multi args support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{io::{Write, stdin, stdout}, process::Command};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // use the '$ ' char as the prompt
+        // need to explicitly flush this to ensure it prints before read_line
+        print!("$ ");
+        stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        // everything after the first whitespace char
+        // is interpreted as args to the command
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        let mut child = Command::new(command)
+            .args(args)
+            .spawn()
+            .unwrap();
+
+        // dont accept another command until this one completes
+        child.wait();
+    }
 }
